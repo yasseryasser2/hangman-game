753020a17b184d76f3318351c5918f3de6267d3f
Yasser Algburi <yasseralgburi@gmail.com>
2025-09-15 15:18:03 -0400

    handle guess logic

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const keyboard = document.getElementById("keyboard");
 const wordDisplay = document.getElementById("word-display");
+const wrongLetters = document.getElementById("wrong-letters");
+const guessLeft = document.getElementById("guesses-left");
 
 let words = [
-  "Football",
-  "Crispy",
-  "Naruto",
-  "Blueberrie",
-  "Twentyonepilots",
+  "football",
+  "crispy",
+  "naruto",
+  "blueberrie",
+  "twentyonepilots",
   "hayat",
 ];
 
+let guessCounter = 5;
 // will implement soon
 let correctLetters = [];
 let incorrectLetters = [];
@@ -18,7 +21,8 @@ let randomElement = Math.floor(Math.random() * words.length);
 const randomWord = words[randomElement];
 console.log(randomWord);
 
-let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
 alphabet.forEach((letter) => {
   const button = document.createElement("button");
   button.innerText = letter;
@@ -27,16 +31,41 @@ alphabet.forEach((letter) => {
   keyboard.append(button);
 });
 
-// Handle the guess logic not implemented yet
 function handleGuess(letter, button) {
-  console.log("You clicked:", letter);
+  let foundMatch = false;
+
+  for (let i = 0; i < randomWord.length; i++) {
+    let currentChar = randomWord[i];
+    if (currentChar === letter) {
+      displayWord[i] = letter;
+      foundMatch = true;
+    }
+  }
+
+  if (foundMatch) {
+    if (!correctLetters.includes(letter)) {
+      correctLetters.push(letter);
+    }
+    displayThyWord();
+  } else {
+    if (!incorrectLetters.includes(letter)) {
+      incorrectLetters.push(letter);
+      guessCounter--;
+    }
+    wrongLetters.innerText = "Wrong letters: " + incorrectLetters.join(", ");
+    guessLeft.innerText = guessCounter;
+  }
+
   button.disabled = true;
 }
 
-let displayWord = ["_", "_", "_", "_", "_", "_"];
+console.log(correctLetters);
+// Handle how the game victory condition is. When the player gusses all correct letters before 5 incorrect attempts he wins
+function handleGame() {}
 
-function renderThyWord() {
+let displayWord = randomWord.split("").map((ch) => (ch === "" ? "" : "_"));
+
+function displayThyWord() {
   wordDisplay.innerText = displayWord.join("");
 }
-
-renderThyWord();
+displayThyWord();
